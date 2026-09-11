24ec2c76494601157114a1956f1e1c2f8b002817
ali <ali13741393.ap@gmail.com>
2026-09-11 14:21:17 +0330

    chore: remove old rules/m2

--- src/rules/m2/__init__.py
+++
@@ -1,6 +0,0 @@
-"""قوانین فاز m2 - adjective"""
-from .more_most_rule    import MoreMostRule
-from .little_rule       import LittleRule
-from .hyphenated_rule   import HyphenatedRule
-
-__all__ = ['MoreMostRule', 'LittleRule', 'HyphenatedRule']
