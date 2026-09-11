53ac5e7c9f7cfad62460ae45df42e6d6a17119b0
ali <ali13741393.ap@gmail.com>
2026-09-11 15:55:59 +0330

    chore: export new postprocess rules from package __init__

--- src/rules/np/m1/__init__.py
+++ src/rules/np/m1/__init__.py
@@ -1,6 +1,12 @@
-"""قوانین فاز m1 (NP) - determiner / quantifier / عدد"""
+"""قوانین فاز m1 (NP)"""
 from .some_number_rule import SomeNumberRule
 from .double_m1_rule import DoubleM1Rule
+from .double_quantifier_rule import DoubleQuantifierRule
 from .m1_after_noun_rule import M1AfterNounRule
 
-__all__ = ['SomeNumberRule', 'DoubleM1Rule', 'M1AfterNounRule']
+__all__ = [
+    'SomeNumberRule',
+    'DoubleM1Rule',
+    'DoubleQuantifierRule',
+    'M1AfterNounRule',
+]
--- src/rules/np/special/__init__.py
+++ src/rules/np/special/__init__.py
@@ -1,9 +1,22 @@
 """قوانین خاص NP"""
+from .compound_resplit_rule import CompoundResplitRule
+from .m1_of_to_n_rule import M1OfToNRule
+from .after_of_label_rule import AfterOfLabelRule
+from .much_rule import MuchRule
 from .law_sh import LawShRule
 from .law_z import LawZRule
 from .the_ordinal import TheOrdinalRule
 from .final_fix import FinalFixAfterNounRule
 from .wordnet_finalize import WordNetFinalizeRule
-from .much_rule import MuchRule
 
-__all__ = ["LawShRule", "LawZRule", "TheOrdinalRule", "FinalFixAfterNounRule", "WordNetFinalizeRule", "MuchRule"]
+__all__ = [
+    'CompoundResplitRule',
+    'M1OfToNRule',
+    'AfterOfLabelRule',
+    'MuchRule',
+    'LawShRule',
+    'LawZRule',
+    'TheOrdinalRule',
+    'FinalFixAfterNounRule',
+    'WordNetFinalizeRule',
+]
