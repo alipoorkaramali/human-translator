b9aac440eaff53ffd35691f13958904b67537ce0
ali <ali13741393.ap@gmail.com>
2026-09-11 14:21:31 +0330

    chore: remove last old rules/m4 path

--- src/rules/m4/__init__.py
+++
@@ -1,5 +0,0 @@
-"""قوانین فاز m4 - عدد بعد از اسم / ordinal"""
-from .number_after_noun_rule import NumberAfterNounRule
-from .ordinal_rule           import OrdinalRule
-
-__all__ = ['NumberAfterNounRule', 'OrdinalRule']
